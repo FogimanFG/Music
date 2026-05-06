92e7f8231c6fb52af1b9b2b9f3d28cf717368f32
Fogi <romagorst1217@gmail.com>
2026-05-06 10:10:40 +0300

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,67 @@
+const questions = [
+  {
+    question: "Как правильно объявить переменную в JavaScript (современный способ)?",
+    options: ["var name;", "let name;", "const = name;", "make name;"],
+    correct: 1
+  },
+  {
+    question: "Что выведет console.log(typeof null)?",
+    options: ["'null'", "'object'", "'undefined'", "'number'"],
+    correct: 1
+  },
+  {
+    question: "Какой результат у выражения: '2' + 2 ?",
+    options: ["4", "'22'", "NaN", "Ошибка"],
+    correct: 1
+  },
+  {
+    question: "Что делает оператор === в JavaScript?",
+    options: [
+      "Сравнивает только значения",
+      "Сравнивает тип и значение",
+      "Проверяет равенство строк",
+      "Присваивает значение переменной"
+    ],
+    correct: 1
+  },
+  {
+    question: "Какой результат будет у выражения: Boolean('')?",
+    options: ["true", "false", "undefined", "null"],
+    correct: 1
+  },
+  {
+    question: "Какой метод используется для добавления элемента в конец массива?",
+    options: [".add()", ".append()", ".push()", ".insert()"],
+    correct: 2
+  },
+  {
+    question: "Что делает функция setTimeout()?",
+    options: [
+      "Повторяет действие каждые X миллисекунд",
+      "Выполняет действие через X миллисекунд один раз",
+      "Останавливает выполнение кода",
+      "Очищает консоль"
+    ],
+    correct: 1
+  },
+  {
+    question: "Какое значение вернёт выражение: [] == false ?",
+    options: ["true", "false", "undefined", "Ошибка"],
+    correct: 0
+  },
+  {
+    question: "Как удалить последний элемент массива?",
+    options: [".pop()", ".remove()", ".delete()", ".shift()"],
+    correct: 0
+  },
+  {
+    question: "Что делает JSON.stringify()?",
+    options: [
+      "Преобразует JSON в объект",
+      "Преобразует объект в строку JSON",
+      "Сравнивает объекты",
+      "Форматирует код"
+    ],
+    correct: 1
+  }
+];
